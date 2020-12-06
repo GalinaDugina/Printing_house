d65a617454a5a34de1de499a0a0276a5df772eb6
Галина Дугина <galkaz1977@gmail.com>
2020-12-06 12:46:07 +0600

    #1 изменены файлы настройки приложения

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PH.Models;
+using PH.Services;
 
 namespace PH
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
--- Startup.cs
+++ Startup.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PH.Models;
+using PH.Models.Data;
 
 namespace PH
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+      public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -24,6 +28,22 @@ namespace PH
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<PrintingHouseContext>(options => options.UseNpgsql(connection))
+                .AddIdentity<User, IdentityRole>(options =>
+                {
+                  
+                    options.Password.RequiredLength = 6;   // минимальная длина
+                    options.Password.RequireNonAlphanumeric = false;   // требуются ли не алфавитно-цифровые символы
+                    options.Password.RequireLowercase = false; // требуются ли символы в нижнем регистре
+                    options.Password.RequireUppercase = false; // требуются ли символы в верхнем регистре
+                    options.Password.RequireDigit = false; // требуются ли цифры
+                    options.User.AllowedUserNameCharacters = null;
+                })
+                .AddEntityFrameworkStores<PrintingHouseContext>();
+
+            //services.AddTransient<FileUploadService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +55,7 @@ namespace PH
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Account/Login");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -44,14 +64,14 @@ namespace PH
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
