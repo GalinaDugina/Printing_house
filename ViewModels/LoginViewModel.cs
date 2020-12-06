f948a586d904be8e28bd9ead73ceb31154edb544
Галина Дугина <galkaz1977@gmail.com>
2020-12-06 13:04:24 +0600

    #1 добавлен представление и модель для входа в приложение

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PH.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email/Логин")]
+        public string Login { get; set; }
+           
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+           
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+           
+        public string ReturnUrl { get; set; }
+
+    }
+
+}
