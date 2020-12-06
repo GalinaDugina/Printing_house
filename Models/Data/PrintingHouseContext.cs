91d1ab124586f035cefd42104a990e76f7d222e1
Галина Дугина <galkaz1977@gmail.com>
2020-12-06 12:46:30 +0600

    #1 добавлен контекст базы данных

---
+++ Models/Data/PrintingHouseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PH.Models.Data
+{
+    public class PrintingHouseContext:IdentityDbContext<User>
+    {
+         public DbSet<User> Users { get; set; }
+
+         public PrintingHouseContext(DbContextOptions options) : base(options)
+         {
+         }
+    }
+}
