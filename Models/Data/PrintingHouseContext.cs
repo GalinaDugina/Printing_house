b997264084b1038111045a91841cbae603c2effd
Галина Дугина <galkaz1977@gmail.com>
2020-12-06 14:25:51 +0600

    #2 модели добавлены в контекст БД

--- Models/Data/PrintingHouseContext.cs
+++ Models/Data/PrintingHouseContext.cs
@@ -6,6 +6,11 @@ namespace PH.Models.Data
     public class PrintingHouseContext:IdentityDbContext<User>
     {
          public DbSet<User> Users { get; set; }
+         public DbSet<Order> Orders { get; set; }
+         public DbSet<OrderName> OrderNames { get; set; }
+         public DbSet<Paper> Papers { get; set; }
+         public DbSet<Client> Clients { get; set; }
+         public DbSet<PartOfKit> PartOfKits { get; set; }
 
          public PrintingHouseContext(DbContextOptions options) : base(options)
          {
